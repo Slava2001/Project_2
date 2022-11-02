1515d04819de522ea6924ff69273199be3581f48
Slava <slavakaplya20011501@gmail.com>
2022-11-02 23:14:29 +0300

    add settings module

---
+++ src/settings.cpp
@@ -0,0 +1,10 @@
+#include "settings.hpp"
+
+namespace Settings {
+namespace Window {
+    int height = 200;
+    int width = 200;
+    int fps_limit = 60;
+    const char *title = "Project 2";   
+}
+}
