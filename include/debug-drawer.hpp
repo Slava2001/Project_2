d607a214a4622fe6dad2b24313b3e5991a7d31c4
Slava <slavakaplya20011501@gmail.com>
2022-11-04 14:24:44 +0300

    doxy

--- include/debug-drawer.hpp
+++ include/debug-drawer.hpp
@@ -7,12 +7,21 @@
 #include <string>
 #include <sstream>
 
+/// @brief Static class for show debug info
 class Debug_drawer: public sf::Drawable {
 public:
 
+    /// @brief Add string to debug output.
+    /// @param str string to add.
     static void add_string(const std::string &str);
+    /// @brief Add string whis some value to debug output.
+    /// @tparam T value type.
+    /// @param str value prefix.
+    /// @param val value to show.
     template <typename T>
     static void add_string(const std::string &str, T val);
+    /// @brief Add transparent rectangle to debug output.
+    /// @param rec rectangle to add.
     static void add_rect(const sf::FloatRect &rec);
     
     void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override; 
