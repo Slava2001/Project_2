c4262229e964097d5348d8770c94fb55d144992b
Slava <slavakaplya20011501@gmail.com>
2022-11-04 20:49:59 +0300

    update debug drawer

--- src/debug-drawer.cpp
+++ src/debug-drawer.cpp
@@ -5,6 +5,26 @@
 std::vector<std::string> Debug_drawer::_text_lines;
 std::vector<sf::FloatRect> Debug_drawer::_rects;
 
+template <>
+void Debug_drawer::add_string(const std::string &str, sf::Vector2i val)
+{
+    std::stringstream _sstr;
+    _sstr << str;
+    _sstr << "(" << val.x << "; " << val.y << ")";
+    _text_lines.push_back(_sstr.str());
+    _sstr.clear();
+}
+
+template <>
+void Debug_drawer::add_string(const std::string &str, sf::Vector2f val)
+{
+    std::stringstream _sstr;
+    _sstr << str;
+    _sstr << "(" << val.x << "; " << val.y << ")";
+    _text_lines.push_back(_sstr.str());
+    _sstr.clear();
+}
+
 void Debug_drawer::add_string(const std::string &str)
 {
     _text_lines.push_back(str);
@@ -33,7 +53,7 @@ void Debug_drawer::draw(sf::RenderTarget &target, const sf::RenderStates &states
     tx.setFont(Resources::Fonts::arial);
     tx.setCharacterSize(Settings::Text::debug_text_size);
 
-    for (int i = 0; i < _text_lines.size(); i++)
+    for (std::size_t i = 0; i < _text_lines.size(); i++)
     {
         tx.setString(_text_lines[i]);
         tx.setPosition(sf::Vector2f(0, i * Settings::Text::debug_text_size));
